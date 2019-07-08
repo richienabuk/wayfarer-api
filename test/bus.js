884729b2f26b6a27ffa0baf52bf0a66620ed71e7
Imo-owo Nabuk <richienabuk@gmail.com>
2019-07-08 02:51:30 +0100

    Create a bus for trip
    Create the migration and db schema
    Create the route and controller for adding a new bus
    Remove check for bus id in the see-all buses method

--- test/bus.js
+++ test/bus.js
@@ -1,22 +1,20 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import faker from 'faker';
 import app from '../src/index';
 
 /**
  * Generates a bus and apply the details for test
  */
 const bus = {
-  number_plate: faker.lorem.text(10),
-  manufacturer: faker.company.companyName,
-  model: faker.random.number,
-  year: faker.date,
-  capacity: faker.random.number(12, 48),
+  number_plate: 'AKB849UJ',
+  manufacturer: 'Nabuk',
+  model: 'First Love',
+  year: '1945',
+  capacity: 64,
 };
 
 const should = chai.should();
 chai.use(chaiHttp);
-const { expect } = chai;
 
 /**
  * Test for creating a bus
@@ -34,7 +32,6 @@ describe('/POST Bus create', () => {
         res.body.should.be.a('object');
         res.body.should.have.property('data');
         // eslint-disable-next-line no-unused-expressions
-        expect(res.body.data.id).to.exist;
         res.body.should.have.property('status').eq('success');
         done();
       });
